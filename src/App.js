ee78a2dcc9591d53151d784f4c39f647088c432a
RAHUL RAJ <RAJRAHUL9939@GMAIL.COM>
2020-05-25 17:50:20 +0530

    Seperated the features to different different branch

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = props => {
+    return(
+        <>
+            <h1>Welcome to the landing page for react training Repository</h1>
+            <h3>Features are devided into multiple branch, Switch to appropriate and start hacking.</h3>
+        </>
+    )
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,26 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './apps/context-example/App';
-
-// import App from './apps/todo/App';
-// import { Provider } from 'react-redux';
-// import { createStore } from 'redux';
-// import rootReducer from './apps/todo/store/reducers';
-
-// const store = createStore(rootReducer);
-
-// ReactDOM.render(
-//   <Provider store={store}>
-//     <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-//   </Provider>,
-//   document.getElementById('root')
-// );
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>, document.getElementById('root')
+  </React.StrictMode>, 
+  document.getElementById('root')
 )

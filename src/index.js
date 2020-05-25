51bab59f92474a78e12834583b60735bfefe24f8
RAHUL RAJ <RAJRAHUL9939@GMAIL.COM>
2020-05-25 17:58:55 +0530

    Unused code removed

--- src/index.js
+++ src/index.js
@@ -1,26 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './apps/context-example/App';
 
-// import App from './apps/todo/App';
-// import { Provider } from 'react-redux';
-// import { createStore } from 'redux';
-// import rootReducer from './apps/todo/store/reducers';
+import App from './todo/App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './todo/store/reducers';
 
-// const store = createStore(rootReducer);
-
-// ReactDOM.render(
-//   <Provider store={store}>
-//     <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-//   </Provider>,
-//   document.getElementById('root')
-// );
+const store = createStore(rootReducer);
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
     <App />
-  </React.StrictMode>, document.getElementById('root')
-)
+  </React.StrictMode>
+  </Provider>,
+  document.getElementById('root')
+);

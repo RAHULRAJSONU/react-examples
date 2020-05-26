963bde2a1222013ad588276d6f0a3c58d5e3ecaa
RAHUL RAJ <RAJRAHUL9939@GMAIL.COM>
2020-05-26 10:49:38 +0530

    useEffect example added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
-import React from 'react';
+import React, {useState, useEffect } from 'react';
+import './App.css';
+
+let born =false;
 
 const App = props => {
+    const [growth, setGrowth]=useState(0);
+    const [nirvana, setNirwana] = useState(false);
+
+    const growHandle = () =>{
+        setGrowth(growth+10);
+    };
+
+    useEffect(()=>{
+        console.log('I am born!');
+    },[]);
+
+    useEffect(()=>{
+        if(born){
+            document.title = "nirvana attained";
+        }
+    },[nirvana]);
+
+    useEffect(()=>{
+        if(born)
+            console.log('Make mistake and keep growing!');
+        else
+            born=true;
+        
+        if(growth > 70)
+            setNirwana(true);
+        return ()=>{
+            console.log('Clean up after mistakes.')
+        };
+    });
+
     return(
         <div className="App">
             <header className="App-header">
-                <h1>Welcome to the landing page for react training Repository</h1>
+                <h1>Use Effect</h1>
+                <h3>Growth:{growth}</h3>
+                <button onClick={growHandle}>Learn and grow!</button>
             </header>
-            <h3>Features are devided into multiple branch, Switch to appropriate and start hacking.</h3>
         </div>
     )
 }
